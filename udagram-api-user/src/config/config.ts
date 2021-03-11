773ee7cf1e619152beddfb5f92568014de6ff434
sebulibah <sebuliba.hannah@live.com>
2021-03-11 08:50:33 +0300

    fix: fix type error

--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -11,10 +11,10 @@ export const config = {
     "url": process.env.URL
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DB,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres"
   },
   "jwt": {
